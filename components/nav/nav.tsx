ddfcac5a3d7af98d394b97d90346f32a99c9e833
herekim <khere220@gmail.com>
2022-11-24 19:23:14 +0900

    feat: Add Icon component

--- components/nav/nav.tsx
+++ components/nav/nav.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faUser } from '@fortawesome/free-solid-svg-icons';
+import Icon from '../icon';
 
 const Nav = () => {
   return (
     <nav className="flex h-12 items-center justify-between border-b border-main-border px-5">
-      <button>
-        <FontAwesomeIcon
-          icon={faBars}
-          style={{ color: '#808080', height: '25px' }}
-        />
-      </button>
-      <button>
-        <FontAwesomeIcon
-          icon={faUser}
-          style={{ color: '#808080', height: '25px' }}
-        />
-      </button>
+      <Icon icon="bars"></Icon>
+      <Icon icon="user"></Icon>
     </nav>
   );
 };
