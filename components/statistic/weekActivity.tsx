77fbccfcb300482525b2fc40da255747f6f4fba7
herekim <khere220@gmail.com>
2022-11-28 18:52:13 +0900

    feat: Add statistic page view

---
+++ components/statistic/weekActivity.tsx
@@ -0,0 +1,11 @@
+const WeekActivity = () => {
+  return (
+    <div className="w-full py-6">
+      <div className="h-56 rounded-lg bg-main-blue p-5 text-lg text-white">
+        <h2>주간 흐름</h2>
+      </div>
+    </div>
+  );
+};
+
+export default WeekActivity;
