0e7bc5dcf676984f72b13683c0e94e957ee70fcc
alwayshere <76519867+herekim@users.noreply.github.com>
2022-12-23 20:08:34 +0900

    feat: Modify SideTap Modal

--- components/modal/sideTap.tsx
+++ components/modal/sideTap.tsx
@@ -1,77 +1,75 @@
 import { useEffect, useRef } from 'react';
 
-import Portal from 'components/portal';
-import Blur from 'components/modal/dim';
 import Target from 'components/svgs/target.svg';
 import BoxOpen from 'components/svgs/boxOpen.svg';
 import ChartPie from 'components/svgs/chartPie.svg';
 import SquareCheck from 'components/svgs/squareCheck.svg';
 import User from 'components/svgs/user.svg';
 import Key from 'components/svgs/key.svg';
+import useModal from 'customs/useModal';
 
 import Link from 'next/link';
 
 import useOutsideClick from 'customs/useOutsideClick';
 
-import { OnClickProps } from 'types/props';
-
-const SideTap = ({ onClick }: OnClickProps) => {
+const SideTap = () => {
   const modalRef = useRef<HTMLDivElement>(null);
   const { clicked } = useOutsideClick(modalRef);
+  const { closeModal } = useModal();
+
+  const closeSideTap = () => {
+    closeModal({ element: <SideTap /> });
+  };
 
   useEffect(() => {
-    if (clicked) onClick();
+    if (clicked) closeSideTap();
   }, [clicked]);
 
   return (
-    <Portal>
-      <Blur>
-        <div
-          ref={modalRef}
-          className="flex h-full w-3/5 flex-col gap-10 rounded-tr-lg rounded-br-lg bg-white p-10"
-        >
-          <h1 className="text-3xl font-bold">디스위크</h1>
-          <ul className="flex flex-col gap-4 text-xl">
-            <Link href={`/`}>
-              <li className="flex items-center gap-4" onClick={() => onClick()}>
-                <Target width="25px" height="25px" fill="#808080" />
-                <button>오늘의 습관</button>
-              </li>
-            </Link>
-            <Link href={`/manage/habit`}>
-              <li className="flex items-center gap-4" onClick={() => onClick()}>
-                <BoxOpen width="25px" height="25px" fill="#808080" />
-                <button>습관 관리</button>
-              </li>
-            </Link>
-            <Link href={`/statistic`}>
-              <li className="flex items-center gap-4" onClick={() => onClick()}>
-                <ChartPie width="25px" height="25px" fill="#808080" />
-                <button>습관 데이터</button>
-              </li>
-            </Link>
-            <Link href={`/manage/review`}>
-              <li className="flex items-center gap-4" onClick={() => onClick()}>
-                <SquareCheck width="25px" height="25px" fill="#808080" />
-                <button>회고 관리</button>
-              </li>
-            </Link>
-            <Link href={`/user`}>
-              <li className="flex items-center gap-4" onClick={() => onClick()}>
-                <User width="25px" height="25px" fill="#808080" />
-                <button>유저 정보</button>
-              </li>
-            </Link>
-            <Link href={`/setting`}>
-              <li className="flex items-center gap-4" onClick={() => onClick()}>
-                <Key width="25px" height="25px" fill="#808080" />
-                <button>설정</button>
-              </li>
-            </Link>
-          </ul>
-        </div>
-      </Blur>
-    </Portal>
+    <div
+      ref={modalRef}
+      className="flex h-full w-3/5 flex-col gap-10 rounded-tr-lg rounded-br-lg bg-white p-10"
+    >
+      <h1 className="text-3xl font-bold">디스위크</h1>
+      <ul className="flex flex-col gap-4 text-xl">
+        <Link href={`/`}>
+          <li className="flex items-center gap-4" onClick={closeSideTap}>
+            <Target width="25px" height="25px" fill="#808080" />
+            <button>오늘의 습관</button>
+          </li>
+        </Link>
+        <Link href={`/manage/habit`}>
+          <li className="flex items-center gap-4" onClick={closeSideTap}>
+            <BoxOpen width="25px" height="25px" fill="#808080" />
+            <button>습관 관리</button>
+          </li>
+        </Link>
+        <Link href={`/statistic`}>
+          <li className="flex items-center gap-4" onClick={closeSideTap}>
+            <ChartPie width="25px" height="25px" fill="#808080" />
+            <button>습관 데이터</button>
+          </li>
+        </Link>
+        <Link href={`/manage/review`}>
+          <li className="flex items-center gap-4" onClick={closeSideTap}>
+            <SquareCheck width="25px" height="25px" fill="#808080" />
+            <button>회고 관리</button>
+          </li>
+        </Link>
+        <Link href={`/user`}>
+          <li className="flex items-center gap-4" onClick={closeSideTap}>
+            <User width="25px" height="25px" fill="#808080" />
+            <button>유저 정보</button>
+          </li>
+        </Link>
+        <Link href={`/setting`}>
+          <li className="flex items-center gap-4" onClick={closeSideTap}>
+            <Key width="25px" height="25px" fill="#808080" />
+            <button>설정</button>
+          </li>
+        </Link>
+      </ul>
+    </div>
   );
 };
 
--- components/navigator/navigator.tsx
+++ components/navigator/navigator.tsx
@@ -1,22 +1,25 @@
 import SideTap from 'components/modal/sideTap';
 import Bars from 'components/svgs/bars.svg';
 import User from 'components/svgs/user.svg';
-import Target from 'components/svgs/target.svg';
-// import BoxOpen from 'components/svgs/boxOpen.svg';
-// import ChartPie from 'components/svgs/chartPie.svg';
-// import SquareCheck from 'components/svgs/squareCheck.svg';
+import useModal from 'customs/useModal';
+import Modal from 'components/modal/modal';
 
 import Link from 'next/link';
 
-import { useState } from 'react';
-
 const Navigator = () => {
-  const [modal, setModal] = useState<boolean>(false);
+  const { openModal } = useModal();
 
   return (
     <nav className="fixed top-0 left-0 right-0 flex h-12 items-center justify-between border-b border-main-border bg-white px-5">
       <div className="flex items-center gap-6">
-        <button className="lg:hidden" onClick={() => setModal((prev) => !prev)}>
+        <button
+          className="lg:hidden"
+          onClick={() =>
+            openModal({
+              element: <SideTap />,
+            })
+          }
+        >
           <Bars width="25px" height="25px" fill="#000000" />
         </button>
         <Link className="hidden lg:block" href={`/`}>
@@ -24,21 +27,18 @@ const Navigator = () => {
         </Link>
         <Link className="hidden lg:block" href={`/manage/habit`}>
           <p>습관관리</p>
-          {/* <BoxOpen width="25px" height="25px" fill="#000000" /> */}
         </Link>
         <Link className="hidden lg:block" href={`/statistic`}>
           <p>습관데이터</p>
-          {/* <ChartPie width="25px" height="25px" fill="#000000" /> */}
         </Link>
         <Link className="hidden lg:block" href={`/manage/review`}>
           <p>회고관리</p>
-          {/* <SquareCheck width="25px" height="25px" fill="#000000" /> */}
         </Link>
       </div>
       <div className="lg:">
         <User width="25px" height="25px" fill="#000000" />
       </div>
-      {modal && <SideTap onClick={() => setModal((prev) => !prev)}></SideTap>}
+      <Modal />
     </nav>
   );
 };
