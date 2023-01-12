23e696ede146de491f4de4c66473dfa926b818b5
alwayshere <76519867+herekim@users.noreply.github.com>
2023-01-12 11:57:59 +0900

    feat: Modify weekNumber component

--- lib/date.ts
+++ lib/date.ts
@@ -2,13 +2,15 @@ import dayjs from 'lib/dayjs';
 
 type RepeatDow = 'mon' | 'tue' | 'wed' | 'thu' | 'fri' | 'sat' | 'sun';
 
-export const getDay = (): RepeatDow => {
+export const getDayShortForm = (): RepeatDow => {
   const day = new Date();
   const week = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
   return week[day.getDay()] as RepeatDow;
 };
 
 export const getWeek = dayjs().week();
+export const getStartDateOfThisWeek = dayjs().weekday(1);
+export const getEndDateOfThisWeek = dayjs().weekday(7);
 export const getYear = dayjs().year();
 export const getDayNumber = dayjs().day();
 export const getLastWeek = dayjs().add(-1, 'week').week();
