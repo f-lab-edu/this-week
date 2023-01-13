c69a80a11f0323419010d5060dfe2123e2b30c42
alwayshere <76519867+herekim@users.noreply.github.com>
2023-01-13 12:01:42 +0900

    fix: Remove duplicate class

--- components/button/writeReviewButton.tsx
+++ components/button/writeReviewButton.tsx
@@ -19,7 +19,7 @@ const WriteReviewButton = () => {
   return (
     <button
       onClick={goToReviewPage}
-      className="w-full rounded-lg bg-main-blue px-5 py-3 text-lg font-semibold font-semibold text-white text-white"
+      className="w-full rounded-lg bg-main-blue px-5 py-3 text-lg font-semibold text-white"
     >
       {getWeek}주차 회고 쓰기 ✏️
     </button>
