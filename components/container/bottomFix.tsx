c343ef92a81ad00a34b1d651fce8fd0d62f4ee12
alwayshere <76519867+herekim@users.noreply.github.com>
2023-01-14 17:54:27 +0900

    feat: Modify BottomFix & Navigator blur

--- components/container/bottomFix.tsx
+++ components/container/bottomFix.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 
 const BottomFix = ({ button }: { button: ReactNode }) => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 flex h-28 items-center justify-center rounded-t-lg bg-white shadow-3xl">
+    <div className="fixed bottom-0 left-0 right-0 flex h-28 items-center justify-center rounded-t-lg shadow-3xl backdrop-blur">
       <div className="w-4/5">{button}</div>
     </div>
   );
